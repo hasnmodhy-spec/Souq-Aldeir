35f6d0a8a193b88f9b479f35da763fba80848708
hasnmodhy-spec <hasnmodhy@gmail.com>
2026-01-02 11:48:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
-// تخزين الإعلانات في localStorage
-let ads = JSON.parse(localStorage.getItem('soug-ads')) || [];
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function saveAd(ad) {
-    ads.push(ad);
-    localStorage.setItem('soug-ads', JSON.stringify(ads));
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAlFgTzlcbaS6NKKlqyOvrxYAnKmxXLTLQ",
+  authDomain: "souq-aldeir.firebaseapp.com",
+  projectId: "souq-aldeir",
+  storageBucket: "souq-aldeir.firebasestorage.app",
+  messagingSenderId: "153018999224",
+  appId: "1:153018999224:web:ddfb7660584941091f6f4d"
+};
 
-function loadAds() {
-    return ads;
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
