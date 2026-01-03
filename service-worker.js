a0d1dbcbce9d2aa17b22224bb699845bddac34a2
hasnmodhy-spec <hasnmodhy@gmail.com>
2026-01-03 10:59:22 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,65 @@
+// service-worker.js
+
+const CACHE_NAME = 'souq-v1';
+
+// الملفات التي سيتم تخزينها مؤقتاً
+const urlsToCache = [
+  '/Souq-Aldeir/index.html',
+  '/Souq-Aldeir/style.css',
+  '/Souq-Aldeir/responsive.css',
+  '/Souq-Aldeir/app.js',
+  '/Souq-Aldeir/auth.js'
+];
+
+// تثبيت Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// تفعيل Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// اعتراض الطلبات
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            
+            const responseToCache = response.clone();
+            
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            
+            return response;
+          });
+      })
+  );
+});
